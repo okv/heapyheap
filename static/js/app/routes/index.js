86a46c445e513147a50c639cce29b2cda80993fa
oleg <oleg.korobenko@gmail.com>
2013-09-15 01:53:53 +0400

    override `route` for redirect anonymous to login page

--- static/js/app/routes/index.js
+++ static/js/app/routes/index.js
@@ -17,7 +17,22 @@ require([
 ) {
 	$(document).ready(function() {
 		var socket = backbone.io.connect();
-		var router = new backbone.Router();
+		var Router = {};
+		var superRoute = backbone.Router.prototype.route;
+		// override `route` for redirect anonymous to login page
+		Router.route = function(route, name, callback) {
+			var oldCallback = callback;
+			callback = function() {
+				if (!this.user && route != 'login') {
+					this.navigate('login', {trigger: true});
+				} else {
+					oldCallback.apply(this, arguments);
+				}
+			}
+			superRoute.call(this, route, name, callback);
+		};
+		Router = backbone.Router.extend(Router);
+		var router = new Router();
 		router.user = null;
 		router.service = new Service({socket: socket});
 		router.service.onLogin = function(user) {

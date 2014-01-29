6d5b30b6cea602d099e3d26eed86993c5ab32926
oleg <oleg.korobenko@gmail.com>
2014-01-29 23:51:46 +0400

    detach previous view automatically

--- static/js/app/routes/task.js
+++ static/js/app/routes/task.js
@@ -4,10 +4,7 @@ define(['app/views/task'], function(TaskView) {
 	var route = {};
 	route.url = 'tasks/:id';
 	route.name = 'task';
-	var view = null;
 	route.callback = function(id) {
-		var self = this;
-		if (view) view.detach();
 		var model = this.collections.tasks.get(id);
 		if (!model) {
 			// TODO: add method for instaces creation
@@ -17,7 +14,7 @@ define(['app/views/task'], function(TaskView) {
 		// always fetch latest model from server before viewing it
 		model.fetch({success: function(model) {
 			console.log('>>> fetched model = ', model.toJSON())
-			view = new TaskView({el: '#task-full', model: model}).render();
+			new TaskView({el: '#task-full', model: model}).render();
 		}});
 	};
 	return route;
--- static/js/app/views/base.js
+++ static/js/app/views/base.js
@@ -6,6 +6,11 @@ define(['backbone', 'app/template'], function(backbone, template) {
 	var View = {};
 
 	View.initialize = function() {
+		// detach previous instance attached to this element, then attach current
+		// TODO; think about detach all views of children dom elements
+		var view = this.$el.data('view');
+		if (view) view.detach();
+		this.$el.data('view', this);
 	};
 
 	View._render = function(templateName, params) {

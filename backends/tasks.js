19147ccfba161bfe76fd18d937829a04e80ad380
oleg <oleg.korobenko@gmail.com>
2014-06-23 23:11:49 +0400

    check lists and hashtags for description

--- backends/tasks.js
+++ backends/tasks.js
@@ -86,8 +86,33 @@ exports.bind = function(backend) {
 		);
 	});
 
+	var renderer = new marked.Renderer(),
+		checkRegExp = /^ *\[([ vx])\]/,
+		checkedHash = {'v': 1, 'x': 1};
+	renderer.listitem = function(text, isOrdered) {
+		var parts = checkRegExp.exec(text);
+		if (parts && parts[1]) {
+			text = text.replace(checkRegExp, '');
+			text = (
+				'<input ' + (checkedHash[parts[1]] ? 'checked="checked"' : '') +
+				' type="checkbox"/>' + text
+			);
+		}
+		return '<li>' + text + '</li>\n';
+	};
+
+	var tagRegExp = /\w+/;
+	var superHeading = renderer.heading;
+	renderer.heading = function(text, level) {
+		if (level === 1 && tagRegExp.test(text)) {
+			return '#' + text;
+		} else {
+			return superHeading.apply(this, arguments);
+		}
+	};
+
 	function createHtmlDescription(description) {
-		return marked(description);
+		return marked(description, {renderer: renderer});
 	}
 
 	return backend;
